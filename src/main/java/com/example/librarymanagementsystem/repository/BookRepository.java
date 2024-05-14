35917fb2a4a424ab5cbcc21ef3c26cfa5dd36ce4
aya250 <aya.moustafa2501@gmail.com>
2024-05-14 22:11:26 +0300

    Step

--- src/main/java/com/example/librarymanagementsystem/repository/BookRepository.java
+++ src/main/java/com/example/librarymanagementsystem/repository/BookRepository.java
@@ -1,5 +1,5 @@
 // BookRepository.java
-package com.yourcompany.librarymanagementsystem.repository;
+package com.example.librarymanagementsystem.repository;
 
 import com.yourcompany.librarymanagementsystem.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
