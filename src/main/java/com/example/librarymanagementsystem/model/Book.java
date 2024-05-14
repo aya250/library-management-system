df9c894ea36da179eebf3752a3066d5dfab3a0af
aya250 <aya.moustafa2501@gmail.com>
2024-05-14 21:34:20 +0300

    The Start Of The Project

---
+++ src/main/java/com/example/librarymanagementsystem/model/Book.java
@@ -0,0 +1,17 @@
+// Book.java
+package com.yourcompany.librarymanagementsystem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    // Add other attributes and getters/setters as needed
+}
